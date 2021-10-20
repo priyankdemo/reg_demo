db166137ab989152e5311e266282b27985ddc309
priyankdemo <85923718+priyankdemo@users.noreply.github.com>
2021-10-20 16:52:12 +0530

    Update train.py

--- train.py
+++ train.py
@@ -37,7 +37,7 @@ if __name__ == "__main__":
         print("Elasticnet model (alpha=%f, l1_ratio=%f):" % (alpha, l1_ratio))
         print("  RMSE: %s" % rmse)
         print("  MAE: %s" % mae)
-        print("  R2: %s" % r2)
+        print("   R2: %s" % r2)
         mlflow.log_param("alpha", alpha)
         mlflow.log_param("l1_ratio", l1_ratio)
         mlflow.log_metric("rmse", rmse)
