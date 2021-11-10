2cb06dc1b7618b0419116fb938ce4537621b4668
priyankdemo <85923718+priyankdemo@users.noreply.github.com>
2021-11-10 22:33:32 +0530

    Update repo-info.py

--- repo-info.py
+++ repo-info.py
@@ -2,7 +2,7 @@
 import requests
 
 # defining the api-endpoint
-API_ENDPOINT = "http://1866-115-119-250-30.ngrok.io/train"
+API_ENDPOINT = "http://3b43-115-119-250-30.ngrok.io/train"
 
 # data to be sent to api
 data = {
