d6720c216d8712ae04b4a158f5b56420aea20736
priyankdemo <85923718+priyankdemo@users.noreply.github.com>
2022-01-24 11:35:51 +0530

    Update repo-info.py

--- repo-info.py
+++ repo-info.py
@@ -8,7 +8,7 @@ API_ENDPOINT = "http://115.115.91.60:5432/train"
 data = {
 	"url": "https://github.com/priyankdemo/reg_demo_priyank_yash",
 	"branch_name": "master",
-	"user_name": "demo_mlops@gmail.com"
+	"user_name": "priy@gmail.com"
 }
 
 # sending post request and saving response as response object
